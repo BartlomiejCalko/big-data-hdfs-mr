3752fad2ff3aea07093de50157095df05328f2c7
Bartlomiej Calko <bartlomiej.calko@gmail.com>
2021-05-24 08:47:40 +0200

    map-step-reduce word checker

---
+++ 01-mrjob/my-map-red.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class MapRedWordCount(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer)
+        ]
+
+    def mapper(self, _, value):
+        yield 'lines ', 1
+        yield 'words ', len(value.split())
+        yield 'chars ', len(value)
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    MapRedWordCount.run()
